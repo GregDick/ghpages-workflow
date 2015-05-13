3de2633f5872e414208482b9d57ae787dfe3ae07
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-05-13 13:54:14 -0500

    copy other files during build

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 #clean and prepare public directory
 rm -rf public
-mkdir public
+cp -r src public
 
 #compile jade to html
 ./node_modules/.bin/jade src -P
@@ -23,4 +23,11 @@ rm -rf src/*.html \
 
 #convert ES6 JS into ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
+
+#clean unneeded files
+rm -rf public_styles \
+       public/*.jade \
+       public/**/*.jade \
+       public/*.scss \
+       public/**/*.scss
 
