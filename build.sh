a20d86b652fa9db3462da73b7c8b3eda7232cd19
Greg Dick <gregory.j.dick@vanderbilt.edu>
2015-05-14 10:20:29 -0500

    fixed errors

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+npm install
+bower install
 
 #clean and prepare public directory
 rm -rf public
@@ -23,6 +25,12 @@ rm -rf src/*.html \
 
 #convert ES6 JS into ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
+
+
+#concat bower_components to public/lib direactor
+if [ -d "bower_components" ]; then
+  ./node_modules/.bin/bowcat . -o public/lib -m
+fi
 
 #clean unneeded files
 rm -rf public_styles \
